eafa7419e98f892a285f6022734330bb70f73387
MehdiRidmani <mahdiridmani22@gmail.com>
2023-08-03 23:39:22 +0100

    done with form field and client side

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,8 +1,28 @@
 
 
-const FormField = () => {
+const FormField = ({ LabelName, type, name, placeholder, value, handleChange, isSurptiseMe, handleSurpriseMe }) => {
     return (
-        <div>FormField</div>
+        <div>
+            <div className="flex items-center gap-2 mb-2 ">
+                <label htmlFor={name} className="block text-sm font-medium text-gray-900">
+                    {LabelName}
+                </label>
+                {isSurptiseMe && (
+                    <button type="button" onClick={handleSurpriseMe}
+                        className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black">
+                        Surprise Me!
+                    </button>
+                )}
+            </div>
+            <input type={type}
+                id={name}
+                name={name}
+                placeholder={placeholder}
+                value={value}
+                onChange={onchange}
+                required
+                className=" bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] border outline-none block w-full p-3" />
+        </div>
     )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,24 @@
 import { useState, useEffect } from "react";
 import { Loader, Card, FormField } from "../components";
 
+
+const RenderCards = ({ data, title }) => {
+    if (data?.length > 0) {
+        return data.map((post) => <Card key={post._id} {...post} />)
+    }
+    return (
+        <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">
+            {title}
+        </h2>
+    )
+};
+
+
+
 const Home = () => {
     const [loading, setLoading] = useState(false);
     const [allPosts, setAllPosts] = useState(null);
-    const [serchText, setSerchText] = useState('');
+    const [searchText, setSearchText] = useState('');
 
     return (
         <section className="max-w-7xl mx-auto">
@@ -24,11 +38,16 @@ const Home = () => {
                     </div>
                 ) : (
                     <>
-                        {serchText && (
+                        {searchText && (
                             <h2 className="font-medium text-[#666e75] text-xl mb-3">
                                 Showing Resuls for <span className="text-[#222328]">{searchText}</span>:
                             </h2>
                         )}
+                        <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+                            {searchText ? (<RenderCards
+                                data={[]} title="No search result found" />) :
+                                (<RenderCards data={[]} title="No Posts found" />)}
+                        </div>
                     </>
                 )}
             </div>
