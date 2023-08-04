f196bc666c454a6b6c315a0c11bcda6cb0f0b54d
MehdiRidmani <mahdiridmani22@gmail.com>
2023-08-04 22:34:23 +0100

    done with open ai api

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -7,3 +7,31 @@ dotenv.config();
 
 const router = express.Router();
 
+const configuration = new Configuration({
+    apiKey: process.env.OPEN_AI_KEY
+})
+
+const openai = new OpenAIApi(configuration);
+
+router.route('/').get((req, res) => {
+    res.send('hello from open ai Api')
+})
+
+router.route('/').post(async (req, res) => {
+    try {
+        const { prompt } = req.body;
+        const aiResponse = await openai.createImage({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json'
+        })
+        const image = aiResponse.data.data[0].b64_json;
+        res.status(200).json({ photo: image })
+    } catch (error) {
+        console.log("Error", error)
+        res.status(404).send(error?.response.data.error.message)
+    }
+})
+
+export default router;
