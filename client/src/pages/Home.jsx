97556e8b9d93d7a87e1e2cd4a188d3409fbfba7e
MehdiRidmani <mahdiridmani22@gmail.com>
2023-08-06 13:30:05 +0100

    post component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,6 +20,29 @@ const Home = () => {
     const [allPosts, setAllPosts] = useState(null);
     const [searchText, setSearchText] = useState('');
 
+    useEffect(() => {
+        const fetchPost = async () => {
+            setLoading(true)
+            try {
+                const response = await fetch('http://localhost:8080/api/v1/post', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                if (response.ok) {
+                    let result = await response.json();
+                    setAllPosts(result.data.reverse())
+                }
+            } catch (error) {
+                alert(error)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchPost()
+    }, [])
+
     return (
         <section className="max-w-7xl mx-auto">
             <div>
@@ -46,7 +69,7 @@ const Home = () => {
                         <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
                             {searchText ? (<RenderCards
                                 data={[]} title="No search result found" />) :
-                                (<RenderCards data={[]} title="No Posts found" />)}
+                                (<RenderCards data={allPosts} title="No Posts found" />)}
                         </div>
                     </>
                 )}
